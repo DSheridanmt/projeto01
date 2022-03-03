cf9c7fda45ea2b364b000e9c39555419e343f819
SafetyLife <78332195+DSheridanmt@users.noreply.github.com>
2022-03-02 23:03:53 -0300

    continuando o tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def index(request):
-    return HttpResponse('Olá mundo, essa é uma resposta HTTP')
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
